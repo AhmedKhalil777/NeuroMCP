fdb50e1b149f98e91aac25dbf5d2d65e8759218d
Ahmed.Khalil <ahmed.khalil@Integrant.com>
2025-05-20 11:41:45 +0300

    Update installer batch file to version 1.1.6

--- install-neuromcp-1.1.6.bat
+++ install-neuromcp-1.1.6.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Uninstalling NeuroMCP.SqlServer...
 dotnet tool uninstall --global NeuroMCP.SqlServer
-echo Installing NeuroMCP.SqlServer version 1.1.4...
-dotnet tool install --global NeuroMCP.SqlServer --version 1.1.4
+echo Installing NeuroMCP.SqlServer version 1.1.6...
+dotnet tool install --global NeuroMCP.SqlServer --version 1.1.6
 echo Done!
 pause 
